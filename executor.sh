a4a52efcb004ed9e411c612ab27c93db3a1feb59
root <Jean.LELIEVRE@supinfo.com>
2019-11-03 21:30:24 +0100

    remove useless executor instruction

--- executor.sh
+++ executor.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.local/bin/
 
 if [[ $2 = "build_script" ]]; then
     CI_COMMIT_REF_NAME=$CUSTOM_ENV_CI_COMMIT_REF_NAME CI_PROJECT_PATH=$CUSTOM_ENV_CI_PROJECT_PATH python3 executor.py $1
